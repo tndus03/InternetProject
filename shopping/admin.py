84b7358fbd53301a9fcf854311d11df745896846
Soo-Yeon Hong <tndus_03@naver.com>
2021-12-16 16:41:49 +0900

    Puzzle 모델 생성하고 시간 설정하기

--- shopping/admin.py
+++ shopping/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from shopping.models import Puzzle
 
 # Register your models here.
+admin.site.register(Puzzle)
--- shopping/models.py
+++ shopping/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class Puzzle(models.Model):
+    title = models.CharField(max_length=50)  # 퍼즐 이름
+    content = models.TextField()  # 퍼즐 설명
+    price = models.IntegerField()  # 가격
+    theme = models.CharField(max_length=30)  # 주제/테마
+    countryM = models.CharField(max_length=30)  #제조국
+
+    create_at = models.DateTimeField(auto_now_add=True)  # 포스팅한 날짜
+    updated_at = models.DateTimeField(auto_now=True)  # 수정된 날짜
+
+    def __str__(self):
+        return f'[{self.pk}] {self.title}'
+
+    # 이미지, 제조사, 카테고리는 나중에
