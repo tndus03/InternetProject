afef1d073726da5e5750748742d1fc4aab8d6a3c
Soo-Yeon Hong <tndus_03@naver.com>
2021-12-19 02:38:20 +0900

    사용자 아바타 보여주기

--- shopping/models.py
+++ shopping/models.py
@@ -67,8 +67,8 @@ class Comment(models.Model):
     def get_absolute_url(self):
         return f'{self.post.get_absolute_url()}#comment-{self.pk}'
 
-    # def get_avatar_url(self):
-    #     if self.author.socialaccount_set.exists():
-    #         return self.author.socialaccount_set.first().get_avatar_url()
-    #     else:
-    #         return 'https://doitdjango.com/avatar/id/383/2df63151053cd6bc/svg/{{self.author.email}}/'
+    def get_avatar_url(self):
+        if self.author.socialaccount_set.exists():
+            return self.author.socialaccount_set.first().get_avatar_url()
+        else:
+            return 'https://doitdjango.com/avatar/id/383/2df63151053cd6bc/svg/{{self.author.email}}/'
